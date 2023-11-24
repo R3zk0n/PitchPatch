9ba7499a8f992931af9c03b207531392e40d6619
Rezkon <rezkon@x64.sh>
2023-11-24 11:23:09 +1100

    Bad Code Sad.

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# This is a sample Python script.
 import sys
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 from modules.msft import msft_module
 from modules.delta import *
@@ -13,17 +10,19 @@ def search_kb_for_cve(cve):
     msft_class.get_kb_for_cve(cve)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # Need to use arch to process and version such as Windows 8 or Windows 7
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--cve", help="CVE to search for")
     parser.add_argument("-a", "--arch", help="Architecture to search for", default="")
     parser.add_argument("-v", "--version", help="Windows version to use", default="")
-    parser.add_argument("-d", "--download", help="Download KB", default=False)
+    parser.add_argument("-d", "--download", help="Download Patch", default=False)
     args = parser.parse_args()
-    if args.cve is None:
-        print("Please provide a CVE to search for")
+    # If no args parse print help
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+   
     elif args.cve is not None and args.download is not None:
         msft_class = msft_module()
         msft_class.odata_query_cve(args.cve, args.version, args.arch, args.download)
